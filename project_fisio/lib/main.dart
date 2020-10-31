b5c47e991281d1980380b3b8f4c3af9a93f8368f
aichatassa <aichatassa@gmail.com>
2020-10-31 13:08:14 -0300

    Update main.dart

--- project_fisio/lib/main.dart
+++ project_fisio/lib/main.dart
@@ -1,42 +1,118 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(FisioApp());
-}
+main() => runApp(FisioApp());
 
 class _FisioAppState extends State<FisioApp> {
   
-  @override
+@override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FisioPort',
-      theme: ThemeData(
-       
-        primarySwatch: Colors.green[200],
-        
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('FisioApp'),
+          backgroundColor: Colors.green,
+        ),
+        body: new Container(
+          //color: Colors.yellowAccent,
+          child: Column(
+            children: <Widget>[
+              new Row(
+                //crossAxisAlignment: CrossAxisAlignment.center,
+                //mainAxisSize: MainAxisSize.min,
+                children: [
+                  Expanded(
+                 child: FlatButton.icon(
+                  padding: const EdgeInsets.all(51),
+                  textColor: Colors.green,
+                  highlightColor: Colors.transparent,
+                  onPressed: () {
+                    // Respond to button press
+                  },
+                  icon: Icon(Icons.person_add, size: 36),
+                  label: Text("Cadastro"),
+                ),
+              ),
+                Expanded(
+                child: FlatButton.icon(
+                  padding: const EdgeInsets.all(51),
+                  textColor: Colors.green,
+                  highlightColor: Colors.transparent,
+                  onPressed: () {
+                    // Respond to button press
+                  },
+                  icon: Icon(Icons.person, size: 36),
+                  label: Text("Reports"),
+                ),
+                )
+               ],
+              ),
+              new Row(
+                 //crossAxisAlignment: CrossAxisAlignment.center,
+                 //mainAxisSize: MainAxisSize.min,
+                children: [
+                Expanded(
+                 child: FlatButton.icon(
+                  padding: const EdgeInsets.all(51),
+                  textColor: Colors.green,
+                  highlightColor: Colors.transparent,
+                  onPressed: () {
+                    // Respond to button press
+                  },
+                  icon: Icon(Icons.today, size: 36),
+                  label: Text("Agenda"),
+                ),
+                ),
+                Expanded(
+                child: FlatButton.icon(
+                  padding: const EdgeInsets.all(51),
+                  textColor: Colors.green,
+                  highlightColor: Colors.transparent,
+                  onPressed: () {
+                    // Respond to button press
+                  },
+                  icon: Icon(Icons.library_books, size: 36),
+                  label: Text("Fichas"),
+                ),
+              ),
+               ],
+              ),
+               new Row(
+                //crossAxisAlignment: CrossAxisAlignment.center,
+                 //mainAxisSize: MainAxisSize.min,
+                children: [
+                Expanded(
+                 child: FlatButton.icon(
+                  padding: const EdgeInsets.all(51),
+                  textColor: Colors.green,
+                  highlightColor: Colors.transparent,
+                  onPressed: () {
+                    // Respond to button press
+                  },
+                  icon: Icon(Icons.settings, size: 36),
+                  label: Text("Settings"),
+                ),
+                ),
+                Expanded(
+                child: FlatButton.icon(
+                  padding: const EdgeInsets.all(51),
+                  textColor: Colors.green,
+                  highlightColor: Colors.transparent,
+                  onPressed: () {
+                    // Respond to button press
+                  },
+                  icon: Icon(Icons.exit_to_app, size: 36),
+                  label: Text("Sair"),
+                ),
+              )
+               ],
+              ),
+            ],
+          ),
+        ),
+      ), 
     );
   }
 }
-
-  @override
-
-
-  @override
-  Widget build(BuildContext context) {
-    
-    return Scaffold(
-      appBar: AppBar(
-       title: Text('FisioApp'),
-      ),
-      body: Center(
-      
-       
-      ),
-    );
-  }
-
 class FisioApp extends StatefulWidget{
   _FisioAppState createState(){
     return _FisioAppState();
